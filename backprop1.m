ff5df231d33159ee2df91c0a2634e62a014ca8de
urszulasawicka <urszulasawicka0@gmail.com>
2013-06-02 20:09:57 +0200

    pierwszy wykres

--- backprop1.m
+++ backprop1.m
@@ -59,3 +59,22 @@ for i=1:maxiter
     plot(err);
  
 end
+
+i = 1;
+test_inp = [];
+x = 0;
+while x<=1
+   test_inp(i,1) = train_inp(i,1);
+   test_inp(i,2) = 1;
+   i = i+1;
+   x = x+0.01;
+end;
+
+for i=1:maxiter
+    pred = weight_hidden_output*tanh(test_inp*weight_input_hidden)';
+    error = pred' - test_inp(:,1);
+    err1(i) =  (sum(error.^2))^0.5;
+   
+    figure(1);
+    plot(err1);
+end;
